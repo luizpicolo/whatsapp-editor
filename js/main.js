33a1a9701d41df7b01a1de60049ec718cd06c9fd
Luiz Picolo <luizpicolo@gmail.com>
2021-07-15 11:32:37 -0400

    Correções e melhorias

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
   let input = $("#input-text").whatsappEditor({
     html_content: true
   });
   
   $(document).on('click', '#convert', function() {
     $('#output-text').html(input.getFormattedContent());
+  });
+
+  $(document).on('click', '#output-text', function() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($('#output-text').text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert('Copiado')
   });
 })
